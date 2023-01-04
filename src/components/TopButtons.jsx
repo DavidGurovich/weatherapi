189440fd1f9f33a6360dd442c422964fb3485bc7
David Gurovich <david.gurovich@outlook.com>
2023-01-03 18:42:16 -0800

    added nearby cities for shortcuts

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function TopButtons() {
+    const cities = [
+        {
+            id: 1,
+            title: 'San Diego'
+        },
+
+        {
+            id: 2,
+            title: 'Las Vegas'
+        },
+
+        {
+            id: 3,
+            title: 'Phoenix'
+        },
+
+        {
+            id: 4,
+            title: 'Seattle'
+        },
+
+        {
+            id: 5,
+            title: 'Portland'
+        },
+    ]
+
+  return (
+    <div className='flex items-center justify-around my-6'>
+        {cities.map((city) => (
+            <button key = {city.id} className='text-white text-lg font-medium'>{city.title}</button>
+        ))}
+
+    </div>
+  )
+}
+
+export default TopButtons
