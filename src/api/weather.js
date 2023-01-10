654de9fc1c1b41e348772f7ca7c43ed49288f7e0
David Gurovich <david.gurovich@outlook.com>
2023-01-10 14:14:00 -0800

    api will use fewer details, shorter functions

--- src/api/weather.js
+++ src/api/weather.js
@@ -43,51 +43,51 @@ const formatCurrentWeather = (data) => {
         speed
     };
 }
-const formatForecastWeather = (data) => {
-    let { timezone, daily, hourly} = data;
-    // //daily = daily.slice(1, 6).map((d) => {
-    //     return {
-    //         title: formatToLocalTime(d.dt, timezone, "ccc"),
-    //         temp: d.temp.day,
-    //         icon: d.weather[0].icon,
-    //     };
-    // });
+// const formatForecastWeather = (data) => {
+//     let { timezone, daily, hourly} = data;
+//     // //daily = daily.slice(1, 6).map((d) => {
+//     //     return {
+//     //         title: formatToLocalTime(d.dt, timezone, "ccc"),
+//     //         temp: d.temp.day,
+//     //         icon: d.weather[0].icon,
+//     //     };
+//     // });
 
-    hourly = hourly.slice(1, 6).map((d) => {
-        return {
-            title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
-            temp: d.temp,
-            icon: d.weather[0].icon,
-        };
-    });
+//     hourly = hourly.slice(1, 6).map((d) => {
+//         return {
+//             title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+//             temp: d.temp,
+//             icon: d.weather[0].icon,
+//         };
+//     });
 
-    return { timezone,  hourly };
-};
+//     return { timezone,  hourly };
+// };
 
-const getFormattedWeatherData = async(searchParams) => {
-    const formattedCurrentWeather = await getWeatherData(
-        'weather',
-        searchParams
-    )
-    .then(formatCurrentWeather);
+// const getFormattedWeatherData = async(searchParams) => {
+//     const formattedCurrentWeather = await getWeatherData(
+//         'weather',
+//         searchParams
+//     )
+//     .then(formatCurrentWeather);
 
-    const { lat, lon } = formattedCurrentWeather;
+//     const { lat, lon } = formattedCurrentWeather;
 
-    const formattedForeCastWeather = await getWeatherData('forecast', {
-        lat,
-        lon,
-        exclude: "current, minutely, alerts",
-        units: searchParams.units,
-    }).then(formatForecastWeather);
+//     const formattedForeCastWeather = await getWeatherData('forecast', {
+//         lat,
+//         lon,
+//         exclude: "current, minutely, alerts",
+//         units: searchParams.units,
+//     }).then(formatForecastWeather);
 
-    return { ...formattedCurrentWeather, ...formattedForeCastWeather};
-};
+//     return { ...formattedCurrentWeather, ...formattedForeCastWeather};
+// };
 
-const formatToLocalTime = (
-    secs,
-    zone,
-    format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") =>
-        DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+// const formatToLocalTime = (
+//     secs,
+//     zone,
+//     format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") =>
+//         DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 export default getFormattedWeatherData
 
